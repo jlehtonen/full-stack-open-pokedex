ea3583fa36ea54f2f2436c34b46bc95b256df984
Jaakko Lehtonen <>
2021-11-21 21:50:58 +0200

    Fix error in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "allow",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
